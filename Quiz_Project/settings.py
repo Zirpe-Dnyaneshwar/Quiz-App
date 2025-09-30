33d34662057b9ec5745200bf81dd99bb7194bc78
Zirpe-Dnyaneshwar <zirpepatil@gmail.com>
2025-09-30 16:06:02 +0530

    Add The Template folder and Chenge setting file and create a url and create a view

--- Quiz_Project/settings.py
+++ Quiz_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Quiz_app',
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Quiz_Project.urls'
+import os
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"Template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Quiz_Project/urls.py
+++ Quiz_Project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Quiz_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
+    path('home',views.home)
 ]
