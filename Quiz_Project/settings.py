7186f5cd7452c20cf0b37c08f143ba67d565f790
Zirpe-Dnyaneshwar <zirpepatil@gmail.com>
2025-10-02 10:33:55 +0530

    Create admin pannel and connect to mysql database

--- Quiz_Project/settings.py
+++ Quiz_Project/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'Quiz_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quizprojectdb',
+        'USER':'root',
+        'PASSWORD':'root'
     }
 }
 
--- Quiz_app/admin.py
+++ Quiz_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+admin.site.site_header = "My Quiz App Admin"
+admin.site.site_title = "Quiz App Admin Portal"
+admin.site.index_title = "Welcome to Quiz App Dashboard"
